2bd7986d9c197da15763f7ef24d56ecae5cb1768
wesleypang8 <wpang566@uw.edu>
2016-10-15 23:44:16 -0700

    code to trigger a mac notification

---
+++ src/MacNotification.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import java.io.IOException;
+import java.lang.Runtime;
+
+public class MacNotification {
+
+    /**
+     * displays notification, currently triggered by typing "fix". trigger can/will be changed 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String in = scan.nextLine();
+
+        // JOptionPane.showMessageDialog(null, "fix your posture!", "HEY!",
+        // JOptionPane.ERROR_MESSAGE);
+        Runtime runtime = Runtime.getRuntime();
+        String applescriptCommand = "display notification \"Fix Your Posture\" with title \"Hello\" sound name \"Glass\"";
+
+        String[] args2 = { "osascript", "-e", applescriptCommand };
+        if (in.equals("fix")) {
+            try {
+                Process process = runtime.exec(args2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
